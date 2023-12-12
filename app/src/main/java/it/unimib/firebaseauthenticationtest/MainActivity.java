899c022626f878da6c38927044bc568cd23814c3
Davide Ginelli <ginellidavide@gmail.com>
2023-12-12 23:07:29 +0100

    First commit

---
+++ app/src/main/java/it/unimib/firebaseauthenticationtest/MainActivity.java
@@ -0,0 +1,26 @@
+package it.unimib.firebaseauthenticationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
+    }
+}
